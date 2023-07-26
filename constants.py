07e2d72245484fb87af4ff283832e9ea85bc7394
Ivan Goncharenko <iv.gonch.0907@gmail.com>
2023-07-26 14:54:29 +0300

    formatting files

--- constants.py
+++ constants.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Файл содержит номера строк ENDF
+# Файл содержит номера строк ENDF, физические константы и флаги для выполнения кода
+
+# подробнее про смысл констант см. ENDF6 formats manual 
+# https://www-nds.iaea.org/public/endf/endf-manual.pdf
 
 class ENDF:
     lineMarkup = [0,11,22,33,44,55,66,70,72,75,80]  # разметка строки ENDF. Числа соответствуют номеру первого символа каждого элемента
@@ -9,17 +12,17 @@ class ENDF:
     # ZAP (то же самое для продукта реакции), NBT (?)
     AWRindex = AWPindex = INTindex = IncidentEnergyindex = 1 # номер значения AWR (масса мишени в массах нейтрона), 
     # AWP (то же самое для продукта реакциии), INT (?), IncidentEnergy (энергия влетающей частицы (в данном случае это альфа-частица))
-    JPindex = LIPindex = LANGindex = 2  # номер значения JP (?), LIP (?), LANG (что-то про )
-            # Вот что написано про LANG при LAW=2
+    JPindex = LIPindex = LANGindex = 2  # номер значения JP (?), LIP (?), LANG (что-то про выбор метода интерполяции)
+            # Про LANG при LAW=2 в мануале (стр.158)
             # LANG flag that indicates the representation:
                 # LANG=0, Legendre expansion;
                 # LANG=12, tabulation with pi(µ) linear in µ;   Где pi(µ)= количество простых чисел N : N <= µ
                 # LANG=14, tabulation with log(pi) linear in µ.
 
-            # Вот что написано про LANG в общем описании
-            # Legendre Coefficients Representation (LANG=1)
-            # Kalbach-Mann Systematics Representation (LANG=2)
-            # Tabulated Function Representation (LANG=11-15)
+            # Про LANG в общем описании в мануале (стр.153-157)
+                # Legendre Coefficients Representation (LANG=1)
+                # Kalbach-Mann Systematics Representation (LANG=2)
+                # Tabulated Function Representation (LANG=11-15)
     LCTindex = LAWindex = 3 # номер значения LCT (код системы отсчёта), LAW (вид закона распреедления)
     NKindex = NRindex = NWindex = 4 # номер значения NK (число подсекций в секции  с данным MT),
     # NR (число областей интерполяций), NW (число записей для данной энергии (часто равно NL))
@@ -33,6 +36,8 @@ class ENDF:
     MFindex = 7   # место в строке, на котором стоит значение MF (код типа данных)
     MTindex = 8   # место в строке, на котором стоит значение MT (код типа реакции)
     NSindex = 9   # место в строке, на котором стоит значение MS (номер строки)
+    
+    # верно для всех файлов ENDF6
 
 class physics:
     N_A = 6.0221409e+23
--- processor.py
+++ processor.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Считывает данные конкретного MF и MT из конвертированного файла. 
-# Записывает в папку /processed/ функцию p_i(theta) для каждой энергии налетающей альфа частицы
+# Считывает строки с конкретными MF и MT из конвертированного файла. 
+# Записывает в папку /processed/filename/MF**_MT***/NK**_NE*** функцию p_i(theta) для каждой энергии налетающей альфа частицы
 
 from __future__ import print_function
 from __future__ import division
@@ -21,50 +21,38 @@ def processData(fname, MF, MT):
         print('There is no converted', fname, 'file!', file=sys.stdout)
         converter.convertENDF(fname)
     f = open('converted/' + fname)
-    f1 = open('converted/' + fname)
-    if not os.path.isdir('processed'):
+
+    f1 = open('converted/' + fname) # тут мы просто открываем любой файл. Потом, в цикле, 
+    # будем открывать нужные файлы, когда узнаем их расположение. (Цикл начинается с закрытия файла, для этого нужна эта строка)
+
+    if not os.path.isdir('processed'):  # проверка наличия директории
         os.mkdir('processed')
     
-    if not os.path.isdir('processed/' + fname):
+    if not os.path.isdir('processed/' + fname): # проверка наличия директории
         os.mkdir('processed/' + fname)
 
-    if not os.path.isdir('processed/' + fname + '/MF' + str(MF) + '_MT' + str(MT)):
+    if not os.path.isdir('processed/' + fname + '/MF' + str(MF) + '_MT' + str(MT)): # проверка наличия директории
         os.mkdir('processed/' + fname + '/MF' + str(MF) + '_MT' + str(MT))
 
-    # if not os.path.isdir('processed/' + fname + '/MF' + MF + 'MT' + MT + '/NK' + NK + '_NE' + NE + '/'):
-    #     os.mkdir("processed" + fname + '/MF' + MF + 'MT' + MT + '/NK' + NK + '_NE' + NE)
-
-    # f1 = open('processed/' + fname + '_MF' + str(MF) + '_MT' + str(MT), 'w')
-    
-    # ftest = open('test/' + fname + '_MF' + str(MF) + '_MT' + str(MT), 'w')
-
-    NElineNumber = -1
-    NWlineNumber = [0]
-    tmp = 0
+    NWlineNumber = [0]  # номер подтаблицы (каждая соответствует своей энергии налетающей частицы E_in)
+    tmp = 0     # тк в ENDF таблицы размазанны в строки по 6 элементов, иногда несколько ячеек в строке остаются пустыми.
+                # Эта переменная позволяет не записывать пустые ячейки в память 
     counter = 0 # счётчик номера NW (всего их NE штук)
-    NP = 0
+    NP = 0      # нужен чтобы правильно определять номер строки с NE 
     
-    for line in f.readlines():
-
-        word = np.zeros(10, dtype = float)
-        
+    for line in f.readlines():  # считываем построчно
+        word = np.zeros(10, dtype = float)  # каждый элемент строки файла будем записывать в виде float в массив numpy. Изначально всё - вещественны нули
         if (line[83:85] != ' 0' and line[83:85] != ' 1'):   # отсекаем строки с текстом (те. с MF=' 1' или =' 0', MT=' 451')
-            for i in range(10): # такие сложности нужны чтобы под float() попали только слова из цифр (иначе ошибка)
-                if (line.strip('|').split('|')[i].strip() != ''):   # пустые слова становятся нецелыми нулями, и сохраняются в word[]
+            for i in range(10): # цикл нужен чтобы под float() попали только элементы строки, состоящие из цифр (иначе ошибка)
+                if (line.strip('|').split('|')[i].strip() != ''):   # пустые слова остаются вещественными нулями в word[]
                     word[i] = float(line.strip('|').split('|')[i].strip())  # в конвертированных файлах разделителем является '|' 
-            
-            # ftest.write(str(int(word[constants.ENDF.MFindex])) + '|' + str(int(word[constants.ENDF.MTindex])) + '\n')
 
-            # ftest.write(str(int(word[8]))+'\n')
-            # if (int(word[8]) == MT and int(word[7]) == MF):
-            #     print (MF, MT)
-            #     print('------')
-            #     print (int(word[constants.ENDF.MFindex]), int(word[constants.ENDF.MTindex]))
+        if (int(word[constants.ENDF.MFindex]) == MF and int(word[constants.ENDF.MTindex]) == MT): # читаем только нужные строчки по MF, MT
 
-        if (int(word[constants.ENDF.MFindex]) == MF and int(word[constants.ENDF.MTindex]) == MT): # читаем только нужные строчки
-
-            if (int(word[constants.ENDF.NSindex]) == 1): 
-                # print('a')
+            # ниже просто запоминаем шапку таблицы, может потом пригодится. Вид первых трёх строк всегда одинаков (в MF6 точно)
+            # подробнее про смысл констант см. файл constants.py (краткая справка) или ENDF6 formats manual 
+            # https://www-nds.iaea.org/public/endf/endf-manual.pdf
+            if (int(word[constants.ENDF.NSindex]) == 1):    # ищем NK. Он всегда 
                 ZA = int(word[constants.ENDF.ZAindex])
                 AWR = float(word[constants.ENDF.AWRindex])
                 JP = int(word[constants.ENDF.JPindex])
@@ -72,48 +60,38 @@ def processData(fname, MF, MT):
                 NK = int(word[constants.ENDF.NKindex])
             
             if (int(word[constants.ENDF.NSindex]) == 2):    # ищем NP. Он всегда на 2й строчке
-                # print('b')
                 ZAP = int(word[constants.ENDF.ZAPindex])
                 AWP = float(word[constants.ENDF.AWPindex])
                 LIP = int(word[constants.ENDF.LIPindex])
                 LAW = int(word[constants.ENDF.LAWindex])
                 if (LAW != 2): print('LAW != 2 for', fname)
-                # NR = int(word[constants.ENDF.NRindex])    # встречается ещё один NR. Но оба не используются
+                # NR = int(word[constants.ENDF.NRindex])      # встречается ещё один NR. Но оба не используются
                 NP = int(word[constants.ENDF.NPindex])
             
             if (int(word[constants.ENDF.NSindex]) == 3): 
-                # print('c')
-                NBT = int(word[constants.ENDF.NBTindex])  # не уверен что это NBT
-                INT = int(word[constants.ENDF.INTindex])  # не уверен что это INT
+                NBT = int(word[constants.ENDF.NBTindex])    # не уверен что это NBT
+                INT = int(word[constants.ENDF.INTindex])    # не уверен что это INT
             
             # if (int(word[constants.ENDF.NSindex]) <= 3): # вывод шапки таблицы в консоль
-            #     print(word)   
 
-            if (int(word[constants.ENDF.NSindex]) == 4 + math.ceil(NP/3)):   # номер строки с NE
-                # или использовать math.floor
-                # print('NE fere!')
-                NE = int(word[constants.ENDF.NEindex])
-                NElineNumber = int(word[constants.ENDF.NSindex])
-                # counter = 0   счётчик номера NW (всего их NE штук)
-                # NWlineNumber = np.zeros(NE, dtype = int)  # не заработало тк надо инициализировать вне ифа (код не интерпретируется)
-                NWlineNumber[0] = int(NElineNumber + 2)  # номера строк содержащих NW
-                # print(NWlineNumber)
-                # NWlineNumber[counter] = NElineNumber + 1    # номера строк содержащих NW
+            if (int(word[constants.ENDF.NSindex]) == 4 + math.ceil(NP/3)):   # если номер строки таков, то в ней лежит NE
+                NE = int(word[constants.ENDF.NEindex])  # записываем чему равно NE
+                NWlineNumber[0] = int(word[constants.ENDF.NSindex] + 2) # записываем в какой строке ожидать следующее значение NW
 
             if (int(word[constants.ENDF.NSindex]) == int(NWlineNumber[counter])): # номер строоки с очередным NW
-                # print('NW here!')
-                counter += 1
+
+                counter += 1    # счётчик номера E_in                 
                 NWlineNumber.append(NWlineNumber[counter-1] + math.ceil(int(word[constants.ENDF.NWindex])/6) + 1)
-                
+                # записываем в какой строке ожидать следующее значение NW
+
                 f1.close()
 
                 if not os.path.isdir('processed/' + fname + '/MF' + str(MF) + '_MT' + str(MT)):
                     os.mkdir ('processed/' + fname + '/MF' + str(MF) + '_MT' + str(MT))
                 f1 =     open('processed/' + fname + '/MF' + str(MF) + '_MT' + str(MT) + '/NK' + str(NK) + '_NE' + str(counter), 'w')
-
+                # создаём и открываем файл на пути /processed/C13/MF6_MT50/NK1_NE47 (пример)
                 f1.write('MF = ' + str(MF) + '\tMT = ' + str(MT) + '\nAmount of files in directory: ' + str(NE) + '\n')
-                # print('NE found:', NE)
-
+                # записываем MF, MT и число табличек с разными энергиями влетающих частиц E_in
                 f1.write('\nIncident particle energy (eV) = \n' + str(word[constants.ENDF.IncidentEnergyindex]) + '\n\nAmount of points: ' + str(int(word[constants.ENDF.NWindex])) + '\n')
                 # записываем E_in и число точек
 
@@ -123,21 +101,8 @@ def processData(fname, MF, MT):
 
             if (counter > 0 and int(word[constants.ENDF.NSindex]) > NWlineNumber[counter-1]):
                 for i in range(6):
-                    if(i < tmp):    # чтобы не выводить нули из строки, которые являются не значениями, а символами пустых ячеек
-                    # if (int(word[i]) != -1):   # проверка наличия числа в ячейке строки
-                        
+                    if(i < tmp):    # чтобы не выводить нули из строки, которые являются не значениями, а символами пустых ячеек                      
                         f1.write(str(word[i]) + '\n')   # записываем в файл экспериментальное значение без изменений
-                tmp -= 6    # строка прошла, значит количество оставшихся значений уменьшилось на 6
-            # print('weewoo')
+                tmp -= 6    # строка прошла, значит количество оставшихся значений уменьшилось на 6            
 
-            
-
-            # if (word[:4] == [0.]*4):    # строка с заданием NR, NE по правилам начинается с 4х нулей. Использую как маркер. Мб ещё где-то такое встречается
-            #     # NR = word[constants.ENDF.NRindex]   # встречается ещё один NR. Но оба не используются
-            #     NE = word[constants.ENDF.NEindex]
-            #     NSstart = int(word[constants.ENDF.NSindex])  
-            #     print(NR, NE, NSstart)
-            
-            
     f.close()
-    # f1.close()
